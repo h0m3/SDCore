9cff68c8c6318540ee4c1f02396af5519f0268ba
artur <arturpaiva.ds@gmail.com>
2018-11-30 12:47:19 -0200

    Some initialization code optimizations

--- SDCore.cpp
+++ SDCore.cpp
@@ -1,62 +1,74 @@
 #include "SDCore.h"
 
-SDCore::SDCore(byte cs) {
-    pin = cs;
-    low_speed = SPISettings(350000, MSBFIRST, SPI_MODE0);
-    high_speed = SPISettings(20000000, MSBFIRST, SPI_MODE0);
-}
+// TODO:
+// -> Write everything as static methods
+// -> Add full static CRC definition
+// -> Return a array with result
+// -> Send a array with parameters
+// -> Create a define with pin number and SD speed
 
-byte SDCore::begin() {
-    byte r;
-
+bool SDCore::begin() {
     // Setup SPI
     SPI.begin();
     pinMode(pin, OUTPUT);
     digitalWrite(pin, HIGH);
 
     // Wait for SD stabilization
-    SPI.beginTransaction(low_speed);
+    SPI.beginTransaction(SPISettings(
+        350000,
+        MSBFIRST,
+        SPI_MODE0
+    ));
     for (byte i = 10; i--;)
         SPI.transfer(0xFF);
     SPI.endTransaction();
 
     // Send CMD0 (software reset)
     for (byte i = 255; i--;) {
-        if (SDCore::command(0x40, 0, 0x95) == 0x01)
+        if (SDCore::command(CMD0, 0, CRC0) == 0x01)
             break;
     }
 
     // TODO: Add support to Ver 1.x cards
     // Send CMD8 (Check for voltage incompatible)
-    if ((r = SDCore::command(0x48, 0x1AA, 0x87)) != 0x01)
-        return r;
+    if (SDCore::command(0x48, 0x1AA, 0x87) != 0x01)
+        return false;
 
     // Send ACMD41 (Initialize newer cards)
-    for (byte i = 255; i--;) {
-        SDCore::command(0x77, 0, 0x65);
-        if (!(r = SDCore::command(0x69, 0x4000000, 0x77)))
-            return r;
+    byte i = 255;
+    do {
+        if (!i)
+            return false;
+        i--;
         delay(1);
-    }
+        SDCore::command(CMD55, 0, CRC55);
+    } while (SDCore::command(CMD41, 0x4000000, 0x77));
 
     // Send Alternative ACMD41 (older cards)
-    for (byte i = 255; i--;) {
-        SDCore::command(0x77, 0, 0x65);
-        if (!(r = SDCore::command(0x69, 0, 0xE5)))
-            return r;
+    i = 255;
+    do {
+        if (!i)
+            return false;
+        i--;
         delay(1);
-    }
+        SDCore::command(CMD55, 0, CRC55);
+    } while (SDCore::command(CMD41, 0, 0xE5));
 
     // Send CMD1 (older cards)
-    for (byte i = 255; i--;) {
-        if (!(r = SDCore::command(0x41, 0, 0xF9))) {
-            return r;
-        }
+    i = 255;
+    do {
+        if (!i)
+            return false;
+        i--;
         delay(1);
-    }
+    } while (SDCore::command(0x41, 0, 0xF9));
 
-    // Return failed code
-    return r;
+    // Send CMD16 (Set sector length)
+    if (SDCore::command(0x50, 512, 0xFF)) {
+        return false;
+    } // TODO: Calculate CRC
+
+    return !SDCore::command(0x7B, 0, 0xFF); // TODO: Calculate CRC
 }
 
 
@@ -64,7 +76,11 @@ byte SDCore::command(byte command, unsigned long param, byte crc) {
     byte r = 0xFF;
 
     digitalWrite(pin, LOW);
-    SPI.beginTransaction(low_speed);
+    SPI.beginTransaction(SPISettings(
+        350000,
+        MSBFIRST,
+        SPI_MODE0
+    ));
 
     // Wait until device is ready
     for(byte i = 255; i--;)
--- SDCore.h
+++ SDCore.h
@@ -4,15 +4,35 @@
 #include "Arduino.h"
 #include <SPI.h>
 
+// SD Commands Definition
+#define CMD0 0x40
+#define CMD1 0x41
+#define CMD8 0x48
+#define CMD9 0x49
+#define CMD10 0x4A
+#define CMD12 0x4C
+#define CMD16 0x50
+#define CMD17 0x51
+#define CMD18 0x52
+#define CMD23 0x57
+#define CMD24 0x58
+#define CMD25 0x59
+#define CMD41 0x69
+#define CMD55 0x77
+#define CMD58 0x7A
+#define CMD59 0x7B
+
+// SD Commands CRC
+#define CRC0 0x95
+#define CRC1 0xF9
+#define CRC8 0x87
+#define CRC55 0x65
+
 class SDCore {
     public:
-        SDCore(byte cs);
-        byte begin();
+        static bool begin();
     private:
-        byte pin;
-        SPISettings low_speed;
-        SPISettings high_speed;
-        byte command(byte command, unsigned long param, byte crc);
+        static byte command(byte command, unsigned long param, byte crc);
 };
 
 #endif
